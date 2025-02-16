3789e31a9aca5ba5dfc046a5b39ace0cfd9c8745
Denis Galeev <11516397+dengaleev@users.noreply.github.com>
2025-02-16 10:45:48 +0300

    socks5-udp-test (#2)
    * init socks5-udp-test
    
    * better output
    
    * init docs
    
    * remove licence info

---
+++ go/socks5-udp-test/go.mod
@@ -0,0 +1,15 @@
+module github.com/dengaleev/glitch-gate/go/socks5-udp-test
+
+go 1.18
+
+require (
+	github.com/beevik/ntp v1.4.3
+	github.com/txthinking/socks5 v0.0.0-20230325130024-4230056ae301
+)
+
+require (
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/txthinking/runnergroup v0.0.0-20210608031112-152c7c4432bf // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+)
---
+++ go/socks5-udp-test/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"net"
+
+	"github.com/beevik/ntp"
+	"github.com/txthinking/socks5"
+)
+
+var (
+	socks5Address  = flag.String("socks5", "localhost:1080", "Specify the SOCKS5 proxy server in the format 'host:port'.")
+	socks5Username = flag.String("socks5-username", "", "Optional. Username for authentication with the SOCKS5 proxy server.")
+	socks5Password = flag.String("socks5-password", "", "Optional. Password for authentication with the SOCKS5 proxy server.")
+	ntpAddress     = flag.String("ntp-server", "time-a-g.nist.gov:123", "Specify the NTP server in the format 'host:port'.")
+)
+
+const (
+	tcpSOCKS5Timeout = 0 // NTP uses UDP, so this value is not applicable for NTP.
+	udpSOCKS5Timeout = 1000
+)
+
+func main() {
+	flag.Parse()
+
+	socks5Cl, err := socks5.NewClient(
+		*socks5Address,
+		*socks5Username,
+		*socks5Password,
+		tcpSOCKS5Timeout,
+		udpSOCKS5Timeout,
+	)
+	if err != nil {
+		log.Panicf("failed to create SOCKS5 client: %s", err)
+	}
+
+	resp, err := ntp.QueryWithOptions(*ntpAddress, ntp.QueryOptions{
+		Dialer: func(_, remoteAddress string) (net.Conn, error) {
+			return socks5Cl.Dial("udp", remoteAddress)
+		},
+	})
+	if err != nil {
+		log.Panicf("request failed: %s", err)
+	}
+
+	jsonResp, err := json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		log.Panicf("failed to marshal response: %s", err)
+	}
+
+	log.Printf("response: %s", string(jsonResp))
+}
